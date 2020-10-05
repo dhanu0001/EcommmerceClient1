23d03be00e6719b7c3600d9d6dbee82a281f8e6c
poidogho <poidogho@yahoo.com>
2020-10-05 19:44:16 -0400

    test

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "../App";
+import Login from "../components/auth/Login";
 
 it("renders without cashes", () => {
   const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Login />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
