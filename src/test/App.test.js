82651dd62b8535d65a48ff6570da1c46ef94ba73
poidogho <poidogho@yahoo.com>
2020-10-05 20:34:37 -0400

    changes

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,5 +1,17 @@
 import React from "react";
-// import ReactDOM from "react-dom";
-// import App from "../components/App";
+import ReactDOM from "react-dom";
+import App from "../App";
 
-it("renders without crashing", () => {});
+it("renders without crashing", () => {
+  const div = document.createElement(div);
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+// import React from "react";
+// var ReactTestUtils = require("react-dom/test-utils");
+// // import ReactDOM from "react-dom";
+// import App from "../App";
+
+// it("renders without crashing", () => {
+//   ReactTestUtils.isDOMComponent(App);
+// });
