43ff47dae0fc81a0b3033156871f3587e264c0a7
poidogho <poidogho@yahoo.com>
2020-10-11 13:38:37 -0400

    update

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const port = process.env.PORT || 8080;
+
+//Static file declaration
+app.use(express.static(path.join(__dirname, "/build")));
+
+//production mode
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "/build")));
+//   //
+//   app.get("*", (_, res) => {
+//     res.sendFile(path.join(__dirname + "/build/index.html"));
+//   });
+// }
+
+//build mode
+app.get("*", (_, res) => {
+  res.sendFile(path.join(__dirname + "/build/index.html"));
+});
+
+//start server
+app.listen(port, (_) => console.log(`server listening on port: ${port}`));
--- src/util/index.js
+++ src/util/index.js
@@ -4,7 +4,9 @@ import jwtDecode from "jwt-decode";
 const isDevelopment = window.location.hostname.includes("localhost");
 
 const getServer = () => {
-  return isDevelopment ? "http://localhost:5000" : "";
+  return isDevelopment
+    ? "http://localhost:5000"
+    : "https://radiant-land-292215.uc.r.appspot.com";
 };
 
 const decodeUser = () => {
